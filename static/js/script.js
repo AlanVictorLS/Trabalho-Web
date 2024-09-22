5df7e6cba80e7036fdd92908e35a48e2825de568
kaelkkd <kaelsiebra233@gmail.com>
2024-09-22 12:17:03 -0300

    botão de scroll pro topo

---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+/* scroll para o topo*/
+let scrollButton = document.getElementById("scroll-btn");
+scrollButton.addEventListener("click", scrollToTop);
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "block";
+    } 
+    else {
+        scrollButton.style.display = "none"
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0, behavior: "smooth"
+    });
+}
